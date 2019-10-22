fffc6cbc1017aff5ba189c6c63927e55f0274cdf
Kanisha123 <kanishapatel003@yahoo.com>
2019-10-21 19:10:16 -0500

    create controller about

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'about/index'
   get 'trees/index'
   get 'trees/show'
   get 'parks/index'
   get 'parks/show'
+  # root to: 'parks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
