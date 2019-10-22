f991dcb5eb4f7a97661afc6d3b4bcebff6e4f328
Kanisha123 <kanishapatel003@yahoo.com>
2019-10-21 19:17:13 -0500

    add erd image to about page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'trees/show'
   get 'parks/index'
   get 'parks/show'
-  # root to: 'parks#index'
+  root to: 'about#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
