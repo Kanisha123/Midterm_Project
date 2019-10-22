794097cb9337c65107aa32659b1d5f8caec5cb99
Kanisha123 <kanishapatel003@yahoo.com>
2019-10-21 19:06:46 -0500

    create controller trees

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'trees/index'
+  get 'trees/show'
   get 'parks/index'
   get 'parks/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
